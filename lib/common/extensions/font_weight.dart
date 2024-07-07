c9d95c56491f077e22ec674ccc3919d86ac441fe
Roufmajidin <roufmajidin777@gmail.com>
2024-07-08 02:53:21 +0700

    update style

--- lib/common/extensions/font_weight.dart
+++ lib/common/extensions/font_weight.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 extension SetFontWeight on TextStyle {
   /// normal
-  TextStyle setRegular() => copyWith(fontWeight: FontWeight.normal);
+  TextStyle setRegular() => copyWith(fontWeight: FontWeight.normal, fontFamily: 'Pooppins');
   /// w500
-  TextStyle setMedium() => copyWith(fontWeight: FontWeight.w500);
+  TextStyle setMedium() => copyWith(fontWeight: FontWeight.w500,  fontFamily: 'Pooppins');
   /// w600
-  TextStyle setSemiBold() => copyWith(fontWeight: FontWeight.w600);
+  TextStyle setSemiBold() => copyWith(fontWeight: FontWeight.w600,  fontFamily: 'Pooppins');
   /// bold
-  TextStyle setBold() => copyWith(fontWeight: FontWeight.bold);
+  TextStyle setBold() => copyWith(fontWeight: FontWeight.bold,  fontFamily: 'Pooppins');
 }
