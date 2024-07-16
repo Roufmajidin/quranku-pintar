05d064a563b0cbf1d28012755663c8b7a8344ca8
Roufmajidin <roufmajidin777@gmail.com>
2024-07-17 02:47:40 +0700

    update b screen

--- lib/features/main_pages/presentation/materi_belajar/mb_page.dart
+++ lib/features/main_pages/presentation/materi_belajar/mb_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quranku_pintar/core/injection/dependency_injection.dart';
 import 'package:quranku_pintar/features/main_pages/data/usecases/quran_usecase.dart';
-import 'package:quranku_pintar/features/main_pages/presentation/main_screen.dart';
 import 'package:quranku_pintar/features/main_pages/presentation/materi_belajar/mb_screen.dart';
 
 import '../../bloc/main_bloc.dart';
--- lib/features/main_pages/presentation/materi_belajar/mb_screen.dart
+++ lib/features/main_pages/presentation/materi_belajar/mb_screen.dart
@@ -56,8 +56,8 @@ class _MbViewState extends State<MbView> {
       } else {
         AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
         // ignore: use_build_context_synchronously
-         context.read<MainBloc>().add(PostDevice(androidInfo.model));
-         context.read<MainBloc>().add(GetMateriPengguna(androidInfo.model));
+        //  context.read<MainBloc>().add(PostDevice(androidInfo.model));
+        //  context.read<MainBloc>().add(GetMateriPengguna(androidInfo.model));
         print('xxx ${androidInfo.model}');
          
       }
@@ -81,28 +81,38 @@ class _MbViewState extends State<MbView> {
                   panggilMateri();
                   print('oj');
                 },
-                child: Container(
-                    height: 180,
+                child: Stack(
+                  children: [
+                        Container(
+                        height: 180,
+                        width: size.width,
+                        color: AppColors.bg.bg01,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 80, left: 16, right: 16, bottom: 16),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Materi Belajar",
+                                  style: AppTextStyle.body1
+                                      .copyWith(color: AppColors.neutral.ne01)
+                                      .setSemiBold()),
+                              Text(
+                                  "Yuk Belajar, dengan mengikuti kegiatan pada menu ini, kamu akan !",
+                                  style: AppTextStyle.body3
+                                      .copyWith(color: AppColors.neutral.ne01)),
+                            ],
+                          ),
+                        )),
+                        Image.asset(
+                    height: 200,
+                    'assets/images/fly.png',
+                    fit: BoxFit.contain,
                     width: size.width,
-                    color: AppColors.bg.bg01,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 80, left: 16, right: 16, bottom: 16),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Materi Belajar",
-                              style: AppTextStyle.body1
-                                  .copyWith(color: AppColors.neutral.ne01)
-                                  .setSemiBold()),
-                          Text(
-                              "Yuk Belajar, dengan mengikuti kegiatan pada menu ini, kamu akan !",
-                              style: AppTextStyle.body3
-                                  .copyWith(color: AppColors.neutral.ne01)),
-                        ],
-                      ),
-                    )),
+                  )
+                      ],
+                ),
               ),
 
               // content
